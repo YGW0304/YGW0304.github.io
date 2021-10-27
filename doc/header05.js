7f0f8ea85b3f282dce29c5cd69758f3d8f7d202b
YGW0304 <91935168+YGW0304@users.noreply.github.com>
2021-10-27 16:13:13 +0900

    Add files via upload

---
+++ doc/header05.js
@@ -0,0 +1,45 @@
+$(function(){
+//----------------------
+
+//스크롤 할때 헤드변경 addClass
+
+$(window).on('scroll',function(){
+    var sct=$(window).scrollTop();
+    console.log(sct);
+    if(sct>0) {
+        $('#header').addClass('on');
+    }else{
+        $('#header').removeClass('on');
+    }
+})
+
+
+//메인슬라이드
+$('.main_slider').slick({
+    arrows:false,
+    autoplay:true,
+    pauseOnHover:false,
+    //vertical:true, -- 아래위로 움직임.
+    fade:true, // 흐려졌다 나온다.
+});
+
+
+//제품슬라이드
+$('.pr_slider').slick({
+    arrows:false,
+    autoplay:true,
+    slidesToShow: 5,
+    dots:true,
+});
+
+$('#content01 .btn i.xi-arrow-left').on('click',function(){
+    $('.pr_slider').slick('slickPrev');
+});
+$('#content01 .btn i.xi-arrow-right').on('click',function(){
+    $('.pr_slider').slick('slickNext');
+});
+
+
+   
+//-----
+})
--- doc/slick02.js
+++ doc/slick02.js
@@ -8,6 +8,26 @@ $('.main_slider').slick({
     fade:true, // 흐려졌다 나온다.
 });
 
+$('#main_visual span.left').on('click',function(){
+    $('.main_slider').slick('slickPrev');
+});
+$('#main_visual span.right').on('click',function(){
+    $('.main_slider').slick('slickNext');
+});
+
+$('#main_visual .list li').on('click', function(){
+    var idx=$(this).index();
+    console.log(idx);
+    $('.main_slider').slick('slickGoTo',idx);
+    $(this).addClass('on').siblings().removeClass('on')
+})
+
+$('#main_visual .list li').eq(0).addClass('on');
+$('.main_slider').on('afterChange', function(e,s,c){
+    console.log(c);
+    $('#main_visual .list li').eq(c).addClass('on').siblings().removeClass('on');
+    $('#main_visual .photo').css({transform:'rotate('+120*c+'deg'})
+})
 
 //제품슬라이드
 $('.pr_slider').slick({
@@ -21,7 +41,7 @@ $('#content01 .btn i.xi-arrow-left').on('click',function(){
     $('.pr_slider').slick('slickPrev');
 });
 $('#content01 .btn i.xi-arrow-right').on('click',function(){
-    $('.pr_slider').slick('slickNext');
+    $('.pr_slider').slick('slickNext',idx);
 });
 
 
