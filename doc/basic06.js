2703cd15e06d2a67018eacb3dc8599d0e4bb007b
YGW0304 <91935168+YGW0304@users.noreply.github.com>
2021-11-04 15:32:56 +0900

    Add files via upload

--- doc/basic06.js
+++ doc/basic06.js
@@ -29,44 +29,9 @@ $('.pr_slider figure').eq(7).addClass('on');
 $('.pr_slider').on('afterChange', function(e,s,c){
     //c=1,2...
     $('.pr_slider figure').eq(c+7).addClass('on').siblings().removeClass('on');
-    $('#product03 .slide_bar>span').css({left:c*100})
 })
 
 
-$('.pic').slick({
-    arrows:false,
-    autoplay:true,
-    // autoplaySpeed:2000,
-    //centerMode:true,
-    //centerPadding:"100px",
-    pauseOnHover:false,
-    pauseOnFocus:false,
-    asNavFor:'.pic',
-});
-
-
-$('.pr_slider02').slick({
-    arrows:false,
-    slidesToShow:3,
-    autoplay:true,
-    autoplaySpeed:2000,
-    pauseOnHover:false,
-    pauseOnFocus:false,  
-});
-
-
-$('#product04 i.xi-arrow-left').on('click',function(){
-    $('.pr_slider02').slick('slickPrev');
-});
-$('#product04 i.xi-arrow-right').on('click',function(){
-    $('.pr_slider02').slick('slickNext');
-});
-
-
-$('#link select').on('change',function(){
-    var link=$(this).val();
-    if(link)window.open(link)
-});
 // function topBannerClose(){
     // $('#top_banner').hide();
 // }
