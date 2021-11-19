566e60d24e6ed717be67def8336cbd9c72e60167
YGW0304 <91935168+YGW0304@users.noreply.github.com>
2021-11-19 11:07:33 +0900

    Add files via upload

---
+++ doc/gnu01.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        nav:true,
+        dots:true,
+
+    });
+
+
+});
