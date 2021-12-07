bb6965012f274cb6839ceb281758b7c300da3b89
YGW0304 <91935168+YGW0304@users.noreply.github.com>
2021-12-07 10:38:56 +0900

    Add files via upload

--- doc/vertical02.js
+++ doc/vertical02.js
@@ -13,6 +13,25 @@ $(function(){
         $('.main_visual figure').eq(c+1).addClass('on').siblings().removeClass('on');
     });
 
+    $('.mopen').on('click',function(){
+        $('#header').toggleClass('on');
+    });
+
+    $('nav>ul>li>a').on('click', function(){
+        if($('#header').hasClass('on')){
+            $(this).next().slideToggle();
+            $(this).parent().siblings().find('.submenu').slideUp();
+        }
+    });
+    
+    $(window).on('resize', function(){
+        if($(window).width() > 768) {
+            $('#header').removeClass('on');
+            $('.submenu').removeAttr('style');
+        }
+        
+    })
+
 
 });
 
