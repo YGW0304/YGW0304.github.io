47c8a76faf4b5811331c07c6e2c855d3d3576bd7
YGW0304 <91935168+YGW0304@users.noreply.github.com>
2022-02-14 11:31:51 +0900

    Add files via upload

---
+++ pf07/js/pf07.js
@@ -0,0 +1,13 @@
+$(function(){
+    //-------------------------
+    $('.main_slider').slick({
+        arrows:false,
+        autoplay:true,
+        pauseOnHover:false,
+        fade:true, 
+    });
+    
+    
+    
+    //--------------------------
+    })
