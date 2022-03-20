c2f8dd6c3af2e54fb7d38fac8b85aec7e9f49fde
YGW0304 <91935168+YGW0304@users.noreply.github.com>
2022-03-20 22:16:31 +0900

    Add files via upload

---
+++ pf08/js/g_portfolio.js
@@ -0,0 +1,51 @@
+$(function(){
+    //-----------------------------------------------------
+    
+    let sc=$('.section');
+    let sideBar=$('nav li');
+    $('#main').fullpage({
+        anchors:['intro', 'portfolio_01', 'portfolio_02', 'portfolio_03', 'portfolio_04', 'portfolio_05', 'profile'],
+        responsiveWidth: 768,
+        css3:false,
+        afterLoad:function(origin, destination, direction){
+            let idx=destination.index;
+            sc.eq(idx).addClass('on').siblings().removeClass('on');
+            sideBar.eq(idx).addClass('on').siblings().removeClass('on');
+        },
+    });
+    
+    
+ 
+    new Typed('.slogan_txt',{
+        strings: ['Lee Gyuwon', '2022 UX/UI WEB-DESIGN PORTFOLIO'],
+        typeSpeed:100,
+    });
+    $('.cover_btn').on('click', function(){
+        $(this).toggleClass('on');
+        $("#cover").slideToggle(); //fadeToggle
+    });
+    
+    
+    let cloneMenu = $('nav>ul').clone();
+    $('#cover').append(cloneMenu);
+    
+    $('#cover a').on('click', function(){
+        $('#cover').slideUp(); 
+        $('#m_btn').toggleClass('on');
+    });
+    
+    $('#cover').on('scroll wheel touchmove', function(){
+        return false;
+    });
+
+
+    // if($(window).width() > 700) {
+    //     $('#Wrap').sakura();
+    // }
+             
+    var sakura = new Sakura('.sakura_m');
+   
+    
+   
+    //-----------------------------------------------------
+    })
